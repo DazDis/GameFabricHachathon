4ef28685056be73d4cd4298e0ab949a74ec5a6a2
ChiCha <janus-planus@rambler.ru>
2024-10-27 16:50:16 +0300

    Revert "Merge branch 'Ren's-branch' of github.com:DazDis/GameFabricHachathon"
    This reverts commit d429c7c3ae729c41bd6cadf7212a1338a772a614, reversing
    changes made to f26fa6aac69d918acbd23d17f117785f41ba1b64.

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    public float MaxSpeed = 5f;
-    public float MinSpeed = 1f;
+    public float speed = 1f;
 
     void Update()
     {
 
         GameObject bug = GameObject.FindGameObjectWithTag("Bug");
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (bug != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, bug.transform.position, MaxSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MinSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, bug.transform.position, speed * Time.deltaTime);
         }
     }
 }
