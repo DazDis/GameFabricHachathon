c3bde3f0c22b8870d731d3dd7fc5d18085a930ad
Renat Bogdanov <zeloniyosminog@gmail.com>
2024-10-27 18:21:38 +0300

    Обновлена анимация, исправлен ветер

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    public float speed = 1f;
+    public float MaxSpeed = 5f;
+    public float MinSpeed = 1f;
+
 
     void Update()
     {
 
         GameObject bug = GameObject.FindGameObjectWithTag("Bug");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (bug != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, bug.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, bug.transform.position, MaxSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MinSpeed * Time.deltaTime);
         }
     }
 }
